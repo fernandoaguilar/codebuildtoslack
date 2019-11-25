130c5300231c287e544bb7fb4c4c5a4935f88fea
Fernando Aguilar <aguilar-47@hotmail.com>
2019-11-24 22:55:23 -0800

    drop py3.5

--- setup.py
+++ setup.py
@@ -20,14 +20,13 @@ test_requirements = [ ]
 setup(
     author="Fernando Aguilar",
     author_email='aguilarf47@gmail.com',
-    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
