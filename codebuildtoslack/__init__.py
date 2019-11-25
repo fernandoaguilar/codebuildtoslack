26c90fee168920b54474a695df39aebc9a83d3e0
Fernando Aguilar <aguilarf47@gmail.com>
2019-11-24 20:44:19 -0800

    cleanup

--- codebuildtoslack/__init__.py
+++ codebuildtoslack/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for codebuildtoslack."""
 
 __author__ = """Fernando Aguilar"""
-__email__ = 'aguilarf47@gmail.com'
-__version__ = '0.1.0'
+__email__ = "aguilarf47@gmail.com"
+__version__ = "0.1.0"
--- codebuildtoslack/cli.py
+++ codebuildtoslack/cli.py
@@ -3,18 +3,13 @@
 """Console script for codebuildtoslack."""
 import sys
 import click
-import codebuildtoslack
+from codebuildtoslack import codebuildtoslack
 
 
 @click.command()
 def main(args=None):
     """Console script for codebuildtoslack."""
     codebuildtoslack.main()
-
-
-@click.command()
-def test_slack(args=None):
-    codebuildtoslack.test_slack_message()
 
 
 if __name__ == "__main__":
